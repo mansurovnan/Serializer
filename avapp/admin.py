d9bbe92eddae036f5f1f50fdf7d283c99d79b098
user <user@MacBook-Pro-user.local>
2026-09-01 18:28:51 +0500

    done

--- avapp/admin.py
+++ avapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'age', 'phone')
+    search_fields = ('name', 'email')
+    list_filter = ('age',)
